a83ec3a22c496ad3151f51f70983ad4cda828ee3
scherecardoso <aranzacardoso5@gmail.com>
2026-05-29 15:43:07 -0400

    validacionprueba

--- formUsuarios.php
+++ formUsuarios.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.js"></script>
     <title></title>
 
     <style>
